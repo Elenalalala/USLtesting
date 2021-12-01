0d50b683430539804479b3894430b5ab254e43d4
Elena <pengx091@newschool.edu>
2021-11-30 22:43:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const lat = 321;
+const long = 2333;
+
+
+if (window.vuplex) {
+    // The window.vuplex object already exists, so go ahead and send the message.
+    document.addEventListener("click", sending)
+    sendMessageToCSharp();
+  } else {
+    // The window.vuplex object hasn't been initialized yet because the page is still
+    // loading, so add an event listener to send the message once it's initialized.
+    console.log("something wrong with sending");
+  }
+  
+
+function sending(){
+    window.vuplex.postMessage({latitude: lat,longitude: long});
+    console.log("successfully send from javascript");
+}
+
+function addMessageListener() {
+    window.vuplex.addEventListener('message', function(event) {
+      let json = event.data;
+      // > JSON received: { "type": "greeting", "message": "Hello from C#!" }
+      console.log('JSON received: ' + json);
+      let title = document.getElementById('title');
+      title.textContent = json;
+    });
+  }
