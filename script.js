ea7dd5bf3f261880b8a3d7be3c4ffe41fb8a790e
Elena <pengx091@newschool.edu>
2021-11-30 23:18:02 -0500

    change header

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const lat = 321;
 const long = 2333;
-
+let title = document.getElementById('title');
 
 if (window.vuplex) {
     // The window.vuplex object already exists, so go ahead and send the message.
-    document.addEventListener("click", sending)
+    document.addEventListener("click", sending);
     sendMessageToCSharp();
     addMessageListener();
   } else {
     // The window.vuplex object hasn't been initialized yet because the page is still
     // loading, so add an event listener to send the message once it's initialized.
-    console.log("something wrong with sending");
+    title.textContent = "something wrong";
   }
   
 
@@ -24,7 +24,7 @@ function addMessageListener() {
       let json = event.data;
       // > JSON received: { "type": "greeting", "message": "Hello from C#!" }
       console.log('JSON received: ' + json);
-      let title = document.getElementById('title');
+      
       title.textContent = json;
     });
   }
